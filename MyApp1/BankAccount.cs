b5e397262f636f9a7bbfc018e34c0fb92a21847a
Jenny Moon <jenny0moon@gmail.com>
2023-06-21 15:09:28 +0100

    Update Balance property

--- MyApp1/BankAccount.cs
+++ MyApp1/BankAccount.cs
@@ -6,7 +6,19 @@ public class BankAccount
 {
   public string Number { get; }
   public string Owner { get; set; }
-  public decimal Balance { get; }
+  public decimal Balance
+{
+    get
+    {
+        decimal balance = 0;
+        foreach (var item in allTransactions)
+        {
+            balance += item.Amount;
+        }
+
+        return balance;
+    }
+}
 
   private static int accountNumberSeed = 1234567890;
   private List<Transaction> allTransactions = new List<Transaction>();
@@ -17,7 +29,6 @@ public class BankAccount
     accountNumberSeed++;
 
     Owner = name;
-    Balance = initialBalance;
   }
 
   public void MakeDeposit(decimal amount, DateTime date, string note)

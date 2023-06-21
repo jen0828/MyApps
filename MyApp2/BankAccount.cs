1345666dab4ce547c6a18e1ec1fd461b138e80a7
Jenny Moon <jenny0moon@gmail.com>
2023-06-21 15:57:25 +0100

    Complete methods

--- MyApp2/BankAccount.cs
+++ MyApp2/BankAccount.cs
@@ -9,19 +9,32 @@ public class BankAccount
     currentUser = user;
   }
 
-  public Balance()
+  public void Balance()
   {
-
+    Console.WriteLine("Current balance: " + currentUser.Balance);
   }
 
-  public Deposit()
+  public void Deposit()
   {
-
+    Console.WriteLine("How much money do you want to deposit?");
+    double deposit = double.Parse(Console.ReadLine());
+    currentUser.Balance += deposit;
+    Console.WriteLine("Thank you for depositing your money. Your current balance is: " + currentUser.Balance);
   }
 
-  public Withdraw()
+  public void Withdraw()
   {
-
+    Console.WriteLine("How much money do you want to withdraw?");
+    double withdrawal = double.Parse(Console.ReadLine());
+    if (withdrawal > currentUser.Balance)
+    {
+      Console.WriteLine("Insufficient balance :(");
+    }
+    else
+    {
+      currentUser.Balance -= withdrawal;
+      Console.WriteLine("You're good to go! Thank you :)");
+    }
   }
 
 

2d1bd5d7836013366b89b6ab2027c677ed7cc538
Jenny Moon <jenny0moon@gmail.com>
2023-06-21 15:01:06 +0100

    Add BankAccount class

--- MyApp1/Program.cs
+++ MyApp1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyTinyBank;
+
+var account = new BankAccount("Jenny", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");

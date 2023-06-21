525c351535f0e77a9d3edf1b36d084f28de31554
Jenny Moon <jenny0moon@gmail.com>
2023-06-21 15:11:53 +0100

    Complete methods

--- MyApp1/BankAccount.cs
+++ MyApp1/BankAccount.cs
@@ -7,18 +7,18 @@ public class BankAccount
   public string Number { get; }
   public string Owner { get; set; }
   public decimal Balance
-{
+  {
     get
     {
-        decimal balance = 0;
-        foreach (var item in allTransactions)
-        {
-            balance += item.Amount;
-        }
+      decimal balance = 0;
+      foreach (var item in allTransactions)
+      {
+        balance += item.Amount;
+      }
 
-        return balance;
+      return balance;
     }
-}
+  }
 
   private static int accountNumberSeed = 1234567890;
   private List<Transaction> allTransactions = new List<Transaction>();
@@ -29,13 +29,30 @@ public class BankAccount
     accountNumberSeed++;
 
     Owner = name;
+    MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
   }
 
   public void MakeDeposit(decimal amount, DateTime date, string note)
   {
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+    }
+    var deposit = new Transaction(amount, date, note);
+    allTransactions.Add(deposit);
   }
 
   public void MakeWithdrawal(decimal amount, DateTime date, string note)
   {
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+    }
+    if (Balance - amount < 0)
+    {
+      throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+    }
+    var withdrawal = new Transaction(-amount, date, note);
+    allTransactions.Add(withdrawal);
   }
 }

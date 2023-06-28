5e299b72af938249e324f033e7bbb4abf95bcbd6
Jenny Moon <jenny0moon@gmail.com>
2023-06-28 10:59:50 +0100

    Add new class Account

---
+++ MyApp3/Accounting/Account.cs
@@ -0,0 +1,16 @@
+namespace HRapp.Accounting
+{
+  public class Account
+  {
+    private string accountNumber;
+
+    public string AccountNumber
+    {
+      get { return accountNumber; }
+      set
+      {
+        accountNumber = value;
+      }
+    }
+  }
+}

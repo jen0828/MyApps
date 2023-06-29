d3306ade7460a2dcb8c031752c711712fdf01c78
Jenny Moon <jenny0moon@gmail.com>
2023-06-29 10:13:24 +0100

    Add new classes instead of EmployeeType

---
+++ MyApp3/HR/Developer.cs
@@ -0,0 +1,20 @@
+namespace HRapp.HR
+{
+  internal class Developer : Employee
+  {
+    private string currentProject;
+
+    public string CurrentProject
+    {
+      get { return currentProject; }
+      set
+      {
+        currentProject = value;
+      }
+    }
+
+    public Developer(string firstName, string lastName, string email, DateTime birthDay, double? hourlyRate) : base(firstName, lastName, email, birthDay, hourlyRate)
+    {
+    }
+  }
+}
